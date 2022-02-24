9e7a57fada77774c21f6f0ce5df356989ae839b9
maurice chisom <mauricechisom275@gmail.com>
2022-02-25 00:31:16 +0100

    add item and clear functionalities working

--- todo.py
+++ todo.py
@@ -7,8 +7,32 @@ class MAIN(QMainWindow):
         super().__init__()
         loadUi("todo.ui", self)
         self.setWindowTitle("My todo app")
+        self.button_handlers()
 
         self.show()
+
+    def button_handlers(self):
+        self.add_btn.clicked.connect(self.addTodo)
+        self.clear_btn.clicked.connect(self.clr_all)
+        self.del_btn.clicked.connect(self.delItem)
+
+    def addTodo(self):
+        # first we grab d td item from input
+        item = self.todo_input.text()
+        print(item)
+
+        # then add it to d listwidget
+        self.todo_list.addItem(item)
+
+        # then again clr input box
+        self.todo_input.setText("")
+
+    def delItem(self):
+        pass
+
+    def clr_all(self):
+        self.todo_list.clear()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
